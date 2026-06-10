70c5710506bf987051999cbdb508bbfb739e7b2c
Benjamin Meslin <benjamin.msln@outlook.fr>
2026-06-10 18:34:22 +0200

    fix : cache v2 pour forcer mise à jour service worker

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 //  et un fonctionnement partiel hors ligne
 // ============================================================
 
-const CACHE_NAME = 'tags-map-v1';
+const CACHE_NAME = 'tags-map-v2';
 
 // Ressources à mettre en cache au premier chargement
 const STATIC_ASSETS = [
