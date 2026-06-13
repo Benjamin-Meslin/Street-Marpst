35d6ded396e6e027d51852179b758a7ad9e97dbc
Benjamin Meslin <benjamin.msln@outlook.fr>
2026-06-13 12:30:34 +0200

    fix : bouton install toujours visible, instructions adaptées par navigateur

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 //  et un fonctionnement partiel hors ligne
 // ============================================================
 
-const CACHE_NAME = 'tags-map-v10';
+const CACHE_NAME = 'tags-map-v11';
 
 // Ressources à mettre en cache au premier chargement
 const STATIC_ASSETS = [
